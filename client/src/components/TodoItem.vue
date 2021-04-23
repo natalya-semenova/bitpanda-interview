cc14978367f00847fd65227ad2c38dc7042b770a
rodolfoarv <rodolfo@ramirezvalenzuela.com>
2021-04-23 11:51:24 +0300

    Initial implementation of TodoItem

---
+++ client/src/components/TodoItem.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+  .todo-item
+    <div>
+      <li>Test</li>
+    </div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api';
+
+export default defineComponent({
+  name: 'todo-item',
+  data() {
+    return {};
+  },
+});
+
+</script>
+
+<style>
+
+</style>
